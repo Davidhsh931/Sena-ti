f1ccc10fc6c0c4cb69ebb9e24ff912d8e4a04ae8
Aquino <1640762@senati.pe>
2025-10-04 10:52:55 -0500

    Clase Esudiante

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
+use App\Models\Estudiante;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $estudiante = new Estudiante();
+    $estudiante -> nombres = 'Jose';
+    $estudiante -> pri_ape = 'Sanchez';
+    $estudiante -> seg_ape = 'Carrion';
+    $estudiante -> save ();
+
+    return $estudiante;
+
+    //return 'Aqui trabajare';
+
+    //return view('welcome');
 });
+
+Route::get('/saludos', function ()  {
+return 'Hola mi king';
+})->name('saludos');
+
+Route::get('/bienvenidos',  function () {
+    return view('bienvenido');
+})->name('bienvenidos');
+
+Route::get('/dudoso',  function () {
+    return view('dudoso');
+})->name('dudoso');
